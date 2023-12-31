7ab508e33ffa0e6100e0b97ec1faedcdea082449
Nicholas Wege <76549245+nickelsh1ts@users.noreply.github.com>
2023-12-31 18:16:50 -0500

    build(style): bootstrap configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '@app/styles/globals.css';
+import '@app/styles/css/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import styles from 'styles/page.module.css';
+import styles from '@app/styles/css/page.module.css';
 
 export default function Home() {
   return (
