bd8cbf658714af65fcf33f93b6676386e75f4365
Nicholas Wege <76549245+nickelsh1ts@users.noreply.github.com>
2023-12-30 22:47:39 -0500

    build: add pkg dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.(js|ts)x?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+};
